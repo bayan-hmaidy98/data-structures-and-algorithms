965876c16ce2e4f739e3507b8af48073071668a7
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-10-18 08:09:08 +0300

    not finished

--- java/code_challenge05/app/src/main/java/Node.java
+++ java/code_challenge05/app/src/main/java/Node.java
@@ -1,11 +1,29 @@
 package java.code_challenge05.app.src.main.java;
 
 
+import java.awt.color.ICC_Profile;
+
 public class Node <T>{
   Node <T> nextReference;
   public T value;
+  private String data;
 
   public Node(T value){
     this.value = value;
+  }
+
+  public Node getNext() {
+    return nextReference;
+  }
+
+  public void setNext(Node next) {
+    this.nextReference = next;
+  }
+  public String getData() {
+    return data;
+  }
+
+  public void setData(String data) {
+    this.data = data;
   }
 }
