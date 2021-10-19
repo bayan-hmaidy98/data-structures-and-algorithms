bed3cca476db8ac1eb80afd9bf454ea3c55f0f8c
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-10-20 01:00:10 +0300

    add tests

--- java/code_challenge05/app/src/test/java/AppTest.java
+++ java/code_challenge05/app/src/test/java/AppTest.java
@@ -40,16 +40,67 @@ public class AppTest {
   }
 
 
+
+
     @Test
   void testIncludeMethod(){
       assertTrue(testLinkedTest.includes(3), "Node is found");
       assertFalse(testLinkedTest.includes(5), "Node not found");
     }
-    @Test
+
+  @Test public void IndexGreaterThanTheLength(){
+    testLinkedTest.insert("Bayan");
+    testLinkedTest.append("Emad");
+    testLinkedTest.append("Hmaidy");
+
+    String output = testLinkedTest.kth(5);
+    String expected = "Not valid index";
+    assertEquals(expected,output);
+  }
+
+  @Test public void IndexEqualsTheLength(){
+    testLinkedTest.insert("Bayan");
+    testLinkedTest.append("Emad");
+    testLinkedTest.append("Hmaidy");
+
+    String output = testLinkedTest.kth(3);
+    String expected = "Not valid index";
+    assertEquals(expected,output);
+  }
+
+  @Test public void IndexIsNegativeNumber(){
+    testLinkedTest.insert("Bayan");
+    testLinkedTest.append("Emad");
+    testLinkedTest.append("Hmaidy");
+
+    String output = testLinkedTest.kth(-1);
+    String expected = "Not valid index";
+    assertEquals(expected,output);
+  }
+  @Test public void  LinkedListLengthEqualsOne(){
+    testLinkedTest.insert("Bebo");
+
+    String output = testLinkedTest.kth(0);
+    String expected = "Bebo";
+    assertEquals(expected,output);
+  }
+  @Test public void HappyPath(){
+    testLinkedTest.insert("Bayan");
+    testLinkedTest.append("Emad");
+    testLinkedTest.append("Hmaidy");
+
+    String output = testLinkedTest.kth(2);
+    String expected = "Hmaidy";
+    assertEquals(expected,output);
+  }
+
+  @Test
   void testToStringMethod(){
       assertTrue(testLinkedTest.toString() == "{ 3 } -> { 2 } -> { 1 } -> NULL");
     }
 
   }
 
-  
+
+
+
