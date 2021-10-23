093a5edd00d956b41b67592869b7647aef169b78
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-10-23 22:06:15 +0300

    edit code challenge06

---
+++ java/code_challenge05/README.md
@@ -0,0 +1,10 @@
+# Insertions
+
+1. Insert at the end of the linked list by providing the value to insert.
+2. Insert a value before a specified value by giving the value to insert and the value to insert before it. Check if the value to insert before does exist!
+3. Insert a value before a specified value by giving the value to insert and the value to insert after it. Check if the value to insert after does exist!
+
+## Whiteboard Process
+
+
+![](../../../../Pictures/Screenshots/White1.png)![](../../../../Pictures/Screenshots/White2.png)![](../../../../Pictures/Screenshots/White3.png)
--- java/code_challenge05/app/src/test/java/AppTest.java
+++ java/code_challenge05/app/src/test/java/AppTest.java
@@ -24,22 +24,6 @@ public class AppTest {
       testLinkedTest.insert(3);
       assertEquals(this.testLinkedTest.head.value, 3);
     }
-  @Test
-  void headPointer(){
-    assertTrue(this.testLinkedTest.head.value == "3");
-  }
-  @Test
-  void insertMultiple(){
-      int size =0;
-      for(int i = 0; i < 3; i++){
-         test2.insert(i);
-         size++;
-      }
-      assertTrue(size == 2);
-
-  }
-
-
 
 
     @Test
@@ -48,84 +32,35 @@ public class AppTest {
       assertFalse(testLinkedTest.includes(5), "Node not found");
     }
 
-  @Test public void IndexGreaterThanTheLength(){
-    testLinkedTest.insert("Bayan");
-    testLinkedTest.append("Emad");
-    testLinkedTest.append("Hmaidy");
-
-    String output = testLinkedTest.kth(5);
-    String expected = "Not valid index";
-    assertEquals(expected,output);
-  }
-
-  @Test public void IndexEqualsTheLength(){
-    testLinkedTest.insert("Bayan");
-    testLinkedTest.append("Emad");
-    testLinkedTest.append("Hmaidy");
-
-    String output = testLinkedTest.kth(3);
-    String expected = "Not valid index";
-    assertEquals(expected,output);
-  }
-
-  @Test public void IndexIsNegativeNumber(){
-    testLinkedTest.insert("Bayan");
-    testLinkedTest.append("Emad");
-    testLinkedTest.append("Hmaidy");
-
-    String output = testLinkedTest.kth(-1);
-    String expected = "Not valid index";
-    assertEquals(expected,output);
-  }
-  @Test public void  LinkedListLengthEqualsOne(){
-    testLinkedTest.insert("Bebo");
-
-    String output = testLinkedTest.kth(0);
-    String expected = "Bebo";
-    assertEquals(expected,output);
-  }
-  @Test public void HappyPath(){
-    testLinkedTest.insert("Bayan");
-    testLinkedTest.append("Emad");
-    testLinkedTest.append("Hmaidy");
-
-    String output = testLinkedTest.kth(2);
-    String expected = "Hmaidy";
-    assertEquals(expected,output);
-  }
-
-  @Test
-  void zippingLinkedListTest() {
-
-    LinkedList list1 = new LinkedList();
-    list1.append("Bayan");
-    list1.append("Emad");
-    list1.append("Hmaidy");
-
-    LinkedList list2 = new LinkedList();
-    list2.append("Noor");
-    list2.append("Omar");
-    list2.append("Bash");
-
-
-      String testingString = LinkedList.zipLists(list1,list2).toString();
-
-      assertEquals("{ Bayan } -> { Noor } -> { Emad } -> { Omar } -> { Hmaidy } -> { Bash } -> NULL",testingString);
-      assertEquals(list1.toString(),testingString2);
-
-
-
-
-
-    assertThrows(Exception.class, ()->LinkedList.zipLists(list3,list4));
-
-
-  }
 
   @Test
   void testToStringMethod(){
       assertTrue(testLinkedTest.toString() == "{ 3 } -> { 2 } -> { 1 } -> NULL");
     }
+
+    @Test void testAppend(){
+      testLinkedTest.insert(1);
+      testLinkedTest.insert(2);
+      testLinkedTest.insert(3);
+      testLinkedTest.append(4);
+      assertEquals(4, testLinkedTest.size());
+    }
+
+  @Test void testInsertBefore(){
+    testLinkedTest.insert(1);
+    testLinkedTest.insert(2);
+    testLinkedTest.insert(3);
+    testLinkedTest.insertBefore(2, 4);
+    assertTrue(testLinkedTest.toString() == "{ 3 } -> { 4 } -> { 2 } -> { 1 } -> NULL");
+  }
+
+  @Test void testInsertAfter(){
+    testLinkedTest.insert(1);
+    testLinkedTest.insert(2);
+    testLinkedTest.insert(3);
+    testLinkedTest.insertAfter(3, 4);
+    assertTrue(testLinkedTest.toString() == "{ 3 } -> { 4 } -> { 2 } -> { 1 } -> NULL");
+  }
 
   }
 
